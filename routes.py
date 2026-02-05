305ce4d20added817a53ad698374f0481bcee018
DRANetGH35 <144178956+DRANetGH35@users.noreply.github.com>
2026-02-05 09:58:19 -0800

    add: login and logout routes

--- routes.py
+++ routes.py
@@ -1,5 +1,8 @@
-from flask import render_template
 
+from flask import render_template, redirect, request
+from flask_login import current_user, logout_user
+
+from forms import LoginForm
 from extensions import db
 from models import User
 from app import create_app
@@ -8,4 +11,14 @@ app = create_app()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_user=current_user)
+
+@app.route('/log_in')
+def log_in():
+    form = LoginForm()
+    return render_template('log_in.html', form=form)
+
+@app.route('/log_out')
+def log_out():
+    logout_user()
+    return redirect(request.referrer)
